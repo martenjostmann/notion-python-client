19db5f880427fd3a060aff1332ab8f0041e7a203
martenjostmann <marten.jostmann@gmail.com>
2023-10-07 18:10:06 +0200

    #4 test update and remove pages

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,6 +1,6 @@
 import pytest
 
-from notion_python_client.models.properties import *
+from notion_python_client.models.properties import *   # noqa: F403
 from notion_python_client.models.file import File
 from notion_python_client import NPC
 import os
@@ -22,7 +22,8 @@ class TestHandler:
             response_data = json.load(f)
 
         mocker.patch(
-            'notion_python_client.handlers.handler.Handler._make_request', return_value=response_data)
+            'notion_python_client.handlers.handler.Handler._make_request',
+            return_value=response_data)
 
         return mocker
 
@@ -33,31 +34,56 @@ class TestHandler:
             response_data = json.load(f)
 
         mocker.patch(
-            'notion_python_client.handlers.handler.Handler._make_request', return_value=response_data)
+            'notion_python_client.handlers.handler.Handler._make_request',
+            return_value=response_data)
+
+        return mocker
+
+    @pytest.fixture
+    def update_page_mock_requests(self, mocker):
+        dirname = os.path.dirname(__file__)
+        with open(os.path.join(dirname, 'test_data', 'test_update_page.json')) as f:
+            response_data = json.load(f)
+
+        mocker.patch(
+            'notion_python_client.handlers.handler.Handler._make_request',
+            return_value=response_data)
+
+        return mocker
+
+    @pytest.fixture
+    def delete_page_mock_requests(self, mocker):
+        dirname = os.path.dirname(__file__)
+        with open(os.path.join(dirname, 'test_data', 'test_delete_page.json')) as f:
+            response_data = json.load(f)
+
+        mocker.patch(
+            'notion_python_client.handlers.handler.Handler._make_request',
+            return_value=response_data)
 
         return mocker
 
     @pytest.fixture
     def create_page_properties(self):
-        number = Number(id="DtK%3E",
+        number = Number(id="DtK%3E",  # noqa: F405
                         type="number",
                         number=1)
 
-        select_1 = Select(id="DAjW",
+        select_1 = Select(id="DAjW",  # noqa: F405
                           color_="brown",
                           type="select",
                           name="Option1")
 
-        select_2 = Select(id="DAjW",
+        select_2 = Select(id="DAjW",  # noqa: F405
                           color_="green",
                           type="select",
                           name="Option2")
 
-        multi_select = MultiSelect(id="%3Ab%3Ep",
+        multi_select = MultiSelect(id="%3Ab%3Ep",  # noqa: F405
                                    type="multi_select",
                                    multi_select=[select_1, select_2])
 
-        title = Title("My beautiful title")
+        title = Title("My beautiful title")  # noqa: F405
 
         return number, multi_select, title
 
@@ -67,21 +93,27 @@ class TestHandler:
 
         assert page.id == "15e4163e-93ba-40fa-bcc4-0b99b816071b"
 
-    def test_create_page_without_cover(self, client: NPC, create_page_mock_requests, create_page_properties):
+    def test_create_page_without_cover(self,
+                                       client: NPC,
+                                       create_page_mock_requests,
+                                       create_page_properties):
 
         number, multi_select, title = create_page_properties
 
-        properties = PropertiesBase.build_properties(
+        properties = PropertiesBase.build_properties(  # noqa: F405
             {"Number": number, "Multi-select": multi_select, "Title": title})
 
         client.page_handler.create_page(
             "df00e9f8-9fe5-46cb-98c4-7e332a71059f", properties)
 
-    def test_create_page_with_cover_string(self, client: NPC, create_page_mock_requests, create_page_properties):
+    def test_create_page_with_cover_string(self,
+                                           client: NPC,
+                                           create_page_mock_requests,
+                                           create_page_properties):
 
         number, multi_select, title = create_page_properties
 
-        properties = PropertiesBase.build_properties(
+        properties = PropertiesBase.build_properties(  # noqa: F405
             {"Number": number, "Multi-select": multi_select, "Title": title})
 
         cover_url = "www.test.de/image.png"
@@ -89,11 +121,14 @@ class TestHandler:
         client.page_handler.create_page(
             "df00e9f8-9fe5-46cb-98c4-7e332a71059f", properties, cover=cover_url)
 
-    def test_create_page_with_cover_file(self, client: NPC, create_page_mock_requests, create_page_properties):
+    def test_create_page_with_cover_file(self,
+                                         client: NPC,
+                                         create_page_mock_requests,
+                                         create_page_properties):
 
         number, multi_select, title = create_page_properties
 
-        properties = PropertiesBase.build_properties(
+        properties = PropertiesBase.build_properties(  # noqa: F405
             {"Number": number, "Multi-select": multi_select, "Title": title})
 
         file = File(type="external", external={
@@ -101,3 +136,43 @@ class TestHandler:
 
         client.page_handler.create_page(
             "df00e9f8-9fe5-46cb-98c4-7e332a71059f", properties, cover=file)
+
+    def test_create_page_with_cover_dict(self,
+                                         client: NPC,
+                                         create_page_mock_requests,
+                                         create_page_properties):
+
+        number, multi_select, title = create_page_properties
+
+        properties = PropertiesBase.build_properties(  # noqa: F405
+            {"Number": number, "Multi-select": multi_select, "Title": title})
+
+        file = {
+            "type": "external",
+            "external": {
+                "url": "www.test.de/image.png"
+            }
+        }
+
+        client.page_handler.create_page(
+            "df00e9f8-9fe5-46cb-98c4-7e332a71059f", properties, cover=file)
+
+    def test_update_page_with_page_object(self,
+                                          client: NPC,
+                                          page_content,
+                                          update_page_mock_requests):
+
+        client.page_handler.update_page(page=page_content)
+
+    def test_update_page_without_properties(self, client: NPC):
+
+        with pytest.raises(ValueError):
+            client.page_handler.update_page()
+
+    def test_delete_page(self, client: NPC, delete_page_mock_requests):
+
+        page_id = "15e4163e-93ba-40fa-bcc4-0b99b816071b"
+        page = client.page_handler.delete_page(page_id)
+
+        assert page.archived is True
+        assert page.id == page_id
